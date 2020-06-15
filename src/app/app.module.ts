1fad7c8c6c98b1c9a5a5a0549f5ca62eb340e794
ahsn13 <assem.hussein@mni.thm.de>
2020-06-15 11:15:29 +0200

    - Backend connection established - Added Bootstraps Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+// app.module.ts
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { environment } from '../environments/environment';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    NgbModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDekDn6bkOY9I66Nw_ttrA7pX4i7wXATKY',
+    authDomain: 'mycargonaut-b6f0d.firebaseapp.com',
+    databaseURL: 'https://mycargonaut-b6f0d.firebaseio.com',
+    projectId: 'mycargonaut-b6f0d',
+    storageBucket: 'mycargonaut-b6f0d.appspot.com',
+    messagingSenderId: '353710341689',
+    appId: '1:353710341689:web:87b6db09e23c2c7b00d7e8',
+    measurementId: 'G-V98S2HDFE8'
+  }
 };
